1042e63eb7f612da52142d563157ef038a6ba743
Rui.H <huangrui27@icloud.com>
2015-08-15 19:51:22 +0800

    Assignment 1 Complete!
    Turns out to be no more than a few lines.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var history: UILabel!
     @IBOutlet weak var display: UILabel!
     var userIsTheMiddleOfTypingANumber = false
+    @IBAction func clearAll()
+    {
+        history.text = ""
+        displayValue = 0
+        operandStack = []
+    }
     @IBAction func appendDigit(sender: UIButton)
     {
         let digit = sender.currentTitle!
         if userIsTheMiddleOfTypingANumber{
-            display.text = display.text! + digit
+            if digit != "." || display.text!.rangeOfString(".") == nil{
+                display.text = display.text! + digit
+            }
         }else{
             display.text = digit
             userIsTheMiddleOfTypingANumber = true
@@ -24,11 +33,15 @@ class ViewController: UIViewController
 
     }
     var operandStack = Array<Double>()
-    
+
+
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
         if userIsTheMiddleOfTypingANumber{
             enter()
+        }
+        if operation != "π"{
+            history.text = history.text! + operation + "\n"
         }
         switch operation {
         case "+": performOperation { $0 + $1 }
@@ -36,6 +49,9 @@ class ViewController: UIViewController
         case "×": performOperation { $0 * $1 }
         case "÷": performOperation { $1 / $0 }
         case "√": performOperation { sqrt($0)}
+        case "sin": performOperation {sin($0)}
+        case "cos": performOperation {cos($0)}
+        case "π": enter(M_PI)
         default: break
         }
     }
@@ -57,9 +73,13 @@ class ViewController: UIViewController
     @IBAction func enter() {
         userIsTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
+        history.text = history.text! + "\(displayValue)\n"
         print("operandStack = \(operandStack)")
     }
-    
+    private func enter(const:Double) {
+        displayValue = const
+        enter()
+    }
     var displayValue: Double{
         get{
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
